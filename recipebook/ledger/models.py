390299d4e51513fb9d9d72256fe6cd7f776c9ac7
Cowiebungaa <157793629+Cowiebungaa@users.noreply.github.com>
2024-03-04 23:29:18 +0800

    Lab 2

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("ledger:recipe_detail", args=[self.pk])
+    
+    
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(
+        'Ingredient',
+        on_delete=models.CASCADE, 
+        related_name='recipe')
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE,
+        related_name='ingredients')
+    quantity = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return "{} {} for {}".format(self.quantity, self.ingredient, self.recipe.name)
--- recipebook/ledger/urls.py
+++ recipebook/ledger/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import recipe_list, recipe_1, recipe_2 
+from .views import recipe_list, recipe_detail
 
 app_name = "ledger"
 
 urlpatterns = [
-    path('list/', recipe_list, name='recipe_list'),
-    path('1/', recipe_1, name='recipe1_detail'),
-    path('2/', recipe_2, name='recipe2_detail'),
+    path('recipes/', recipe_list, name='recipe_list'),
+    path('recipes/<int:recipe_id>', recipe_detail, name='recipe_detail'),
 ]
